bf85b5bd5b4d85b6a430461f2c0fe0e83892fedd
pikupik <santuyaja51@gmail.com>
2024-09-30 04:31:49 +0700

    fixing done*

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   connectionLimit: 10,
 });
 
